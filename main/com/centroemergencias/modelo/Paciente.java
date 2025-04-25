4ae5debfbcbcbdc6bcf58142c19bbeba10c2099e
bytrick <jucious.vally@gmail.com>
2025-04-25 16:47:55 -0300

    Update Paciente.java
    Clase mas completa

--- main/com/centroemergencias/modelo/Paciente.java
+++ main/com/centroemergencias/modelo/Paciente.java
@@ -1,12 +1,16 @@
 package com.centroemergencias.modelo;
 
+package com.centroemergencias.modelo;
+
 public class Paciente {
     private String nombre;
     private Urgencia urgencia;
+    private Medico medicoAsignado;
 
     public Paciente(String nombre, Urgencia urgencia) {
         this.nombre = nombre;
         this.urgencia = urgencia;
+        this.medicoAsignado = null;
     }
 
     public String getNombre() {
@@ -17,8 +21,20 @@ public class Paciente {
         return urgencia;
     }
 
+    public Medico getMedicoAsignado() {
+        return medicoAsignado;
+    }
+
+    public void asignarMedico(Medico medico) {
+        this.medicoAsignado = medico;
+    }
+
     @Override
     public String toString() {
-        return nombre + " - " + urgencia;
+        return "Paciente{" +
+                "nombre='" + nombre + '\'' +
+                ", urgencia=" + urgencia +
+                ", medicoAsignado=" + (medicoAsignado != null ? medicoAsignado.getNombre() : "sin asignar") +
+                '}';
     }
 }
